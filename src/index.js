c60c4f148547f40619d66c2ff13ff8020aa9cf87
Jason Goebel <Jason.Goebel@gmail.com>
2021-08-06 22:00:53 -0500

    ADD webpack-dev-server and config to hot reload

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return <h1>Music Archive</h1>;
+  return <h1>Welcome to the Music Archive!</h1>;
 };
 
 ReactDOM.render(<App />, document.getElementById('react-entry'));
