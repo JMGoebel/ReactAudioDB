0a381ee3ef58b0ac04746c1ca426b37519865555
Jason Goebel <Jason.Goebel@gmail.com>
2021-08-06 21:52:03 -0500

    ADD React <App /> entry point to 'react-entry'

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log("music list");
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>Music Archive</h1>;
+};
+
+ReactDOM.render(<App />, document.getElementById('react-entry'));
