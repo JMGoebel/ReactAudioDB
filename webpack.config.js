bf3d5006bba69cbf45e288c28c9d07f59124f02f
Jason Goebel <Jason.Goebel@gmail.com>
2021-08-06 21:50:34 -0500

    ADD html-webpack-plugin and config entry point

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,9 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const htmlPlugin = new HtmlWebPackPlugin({
+  template: './src/index.html',
+  filename: './index.html',
+});
+
 module.exports = {
   module: {
     rules: [
@@ -10,4 +16,7 @@ module.exports = {
       },
     ],
   },
-}
+  plugins: [
+    htmlPlugin,
+  ]
+};
